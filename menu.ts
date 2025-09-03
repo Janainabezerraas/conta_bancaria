9dfe3452f2590bd768d7c809c4fe6f244a168e27
Janaína Bezerra <janainabezerra.ti@gmail.com>
2025-09-03 07:37:12 -0300

    Abstract

--- menu.ts
+++ menu.ts
@@ -8,13 +8,6 @@ export function main() {
 
     let opcao: number;
 
-    const conta: Conta = new Conta(1, 123, 1, "Adriana", 1000);
-    conta.visualizar();
-    conta.sacar(10500);
-    conta.visualizar();
-    conta.depositar(5000);
-    conta.visualizar();
-
     const contacorrente = new ContaCorrente(2, 123, 1, "Mariana", 15000, 1000);
     contacorrente.visualizar();
     contacorrente.sacar(2000);
